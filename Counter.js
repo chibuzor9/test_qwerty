f129dc577b6ab49d7d2fcfdafa6c06c9571f1eff
Chibuzor Emmanuel <66841449+chibuzor9@users.noreply.github.com>
2023-05-21 22:21:21 +0100

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,13 @@
+// document.getElementById("count").innerText = count
+
+let count = 0;
+
+
+function increment() {
+    count += 1;
+    // alert(count);
+    document.getElementById("count").innerText = count
+}
+// console.log(count)
+
+
